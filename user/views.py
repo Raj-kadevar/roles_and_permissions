2ba25395d210ed953594391c054a5f58e12f8369
Raj kadevar <raj.kadevar@trootech.com>
2023-09-21 16:37:02 +0530

    comments resolved

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.contrib.auth.models import Group, Permission
+from django.views.generic import ListView
 
 
 class UserLoginView(LoginView):
@@ -13,25 +14,10 @@ class UserLoginView(LoginView):
         return reverse_lazy('home')
 
 
-class GroupListView(LoginRequiredMixin,View):
-    def get(self, request, *args, **kwargs):
-        groups = Group.objects.all().order_by("id")
-        return render(request,'user/home.html',{'groups':groups})
-
-
-class ManagePermissions(LoginRequiredMixin,View):
-    def get(self, request, *args, **kwargs):
-        group_permissions = Group.objects.get(id=kwargs.get('id')).permissions.all()
-        all_permissions = Permission.objects.all()
-        return render(request,"user/edit.html",{'all_permissions':all_permissions,'group_permissions':group_permissions})
-
-    def post(self, request, *args, **kwargs):
-        group_permissions = Group.objects.get(id=kwargs.get('id'))
-        total_permissions = request.POST.getlist('new_selected_checkbox') + request.POST.getlist('selected_checkbox')
-        group_permissions.permissions.set(total_permissions)
-        group_permissions.save()
-        return redirect('home')
-
+class GroupListView(LoginRequiredMixin,ListView):
+    template_name = "user/home.html"
+    queryset = Group.objects.all().order_by("id")
+    context_object_name = "groups"
 
 
 class ViewPermissions(LoginRequiredMixin,View):
